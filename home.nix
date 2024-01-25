da87e6e74db9009c0812864b68b525bd2d412fc5
Yi Ming <ofseed@foxmail.com>
2024-01-25 12:00:02 +0000

    Set catppuccin theme for bat and delta

--- home.nix
+++ home.nix
@@ -115,6 +115,20 @@
     };
     bat = {
       enable = true;
+      config = {
+        theme = "catppuccin";
+      };
+      themes = {
+        catppuccin = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+          file = "Catppuccin-mocha.tmTheme";
+        };
+      };
     };
     lf = {
       enable = true;
@@ -171,6 +185,9 @@
       };
       delta = {
         enable = true;
+        options = {
+          syntax-theme = "catppuccin";
+        };
       };
     };
     gh = {
