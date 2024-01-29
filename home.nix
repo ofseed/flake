489418b30df9255649b0f6211734ef18a86e1602
Yi Ming <ofseed@foxmail.com>
2024-01-29 15:07:31 +0000

    Add function `lfcd`

--- home.nix
+++ home.nix
@@ -71,6 +71,7 @@
 
     ".ideavimrc".source = dotfiles/ideavimrc;
     ".config/lf/icons".source = dotfiles/config/lf/icons;
+    ".config/fish/functions/lfcd.fish".source = dotfiles/config/fish/functions/lfcd.fish;
   };
 
   # Home Manager can also manage your environment variables through
