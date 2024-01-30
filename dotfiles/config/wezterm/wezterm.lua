2702333a731bab75e0f1b62358cd03a6effb460f
Yi Ming <ofseed@foxmail.com>
2024-01-30 17:29:06 +0000

    Adjust WezTerm Window appearance

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -1,11 +1,22 @@
 local wezterm = require "wezterm"
 
 local config = {
+  color_scheme = "Catppuccin Mocha",
   font = wezterm.font "CaskaydiaCove Nerd Font",
+  initial_cols = 100,
+  initial_rows = 45,
+  window_background_opacity = 0.7,
+  window_padding = {
+    left = "0cell",
+    right = "0cell",
+    top = "0cell",
+    bottom = "0cell",
+  },
 }
 
 local windows_config = {
   default_domain = "WSL:NixOS",
+  win32_system_backdrop = "Acrylic",
 }
 
 if wezterm.target_triple:find "windows" then
