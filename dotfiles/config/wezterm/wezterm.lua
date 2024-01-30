0934d01fa72b110ce50bfdd74b8304bb4bef5486
Yi Ming <ofseed@foxmail.com>
2024-01-30 18:33:20 +0000

    Change `default_prog` to fish

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -2,10 +2,12 @@ local wezterm = require "wezterm"
 
 local config = {
   color_scheme = "Catppuccin Mocha",
+
   font = wezterm.font {
     family = "CaskaydiaCove Nerd Font",
     harfbuzz_features = { "calt", "ss01", "ss02" },
   },
+
   initial_cols = 100,
   initial_rows = 45,
   window_background_opacity = 0.7,
@@ -15,9 +17,18 @@ local config = {
     top = "0cell",
     bottom = "0cell",
   },
+
+  default_prog = { "/home/ofseed/.nix-profile/bin/fish" },
 }
 
+local wsl_domains = wezterm.default_wsl_domains()
+
+for _, domains in ipairs(wsl_domains) do
+  domains.default_prog = config.default_prog
+end
+
 local windows_config = {
+  wsl_domains = wsl_domains,
   default_domain = "WSL:NixOS",
   win32_system_backdrop = "Acrylic",
 }
