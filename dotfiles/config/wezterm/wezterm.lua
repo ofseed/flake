28f863055e0c2dd575a2b099db7a404ded19cea4
Yi Ming <ofseed@foxmail.com>
2024-01-30 18:38:22 +0000

    Use `config_builder`

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require "wezterm"
 
-local config = {
+local common_config = {
   color_scheme = "Catppuccin Mocha",
 
   font = wezterm.font {
@@ -24,7 +24,7 @@ local config = {
 local wsl_domains = wezterm.default_wsl_domains()
 
 for _, domains in ipairs(wsl_domains) do
-  domains.default_prog = config.default_prog
+  domains.default_prog = common_config.default_prog
 end
 
 local windows_config = {
@@ -32,6 +32,12 @@ local windows_config = {
   default_domain = "WSL:NixOS",
   win32_system_backdrop = "Acrylic",
 }
+
+local config = wezterm.config_builder()
+
+for k, v in pairs(common_config) do
+  config[k] = v
+end
 
 if wezterm.target_triple:find "windows" then
   for k, v in pairs(windows_config) do
