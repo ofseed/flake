c246d2ec50ec2e9fca391b975c28c16564c53a8b
Yi Ming <ofseed@foxmail.com>
2024-01-30 17:09:22 +0000

    Add Windows specific config for wezterm

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -4,4 +4,14 @@ local config = {
   font = wezterm.font "CaskaydiaCove Nerd Font",
 }
 
+local windows_config = {
+  default_domain = "WSL:NixOS",
+}
+
+if wezterm.target_triple:find "windows" then
+  for k, v in pairs(windows_config) do
+    config[k] = v
+  end
+end
+
 return config
