5c09b7edf255d72bc6ea37fefe2cc277920f0d91
Yi Ming <ofseed@foxmail.com>
2024-04-25 02:02:51 +0800

    Make git auto setup remote

--- home.nix
+++ home.nix
@@ -192,6 +192,9 @@
         init = {
           defaultBranch = "main";
         };
+        push = {
+          autoSetupRemote = true;
+        };
       };
       delta = {
         enable = true;
