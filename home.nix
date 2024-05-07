4cbee3a76ff20c0c1b92e54c4e7e92d10a043e4a
Yi Ming <ofseed@foxmail.com>
2024-05-07 21:12:23 +0800

    Remove no longer needed workaround

--- home.nix
+++ home.nix
@@ -179,10 +179,6 @@
     };
     gh = {
       enable = true;
-      settings = {
-        # Workaround for https://github.com/nix-community/home-manager/issues/4744
-        version = 1;
-      };
     };
     gh-dash = {
       enable = true;
