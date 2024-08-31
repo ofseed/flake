aeb11eb235b28ad0497ddbc0beb127a24d78acb5
Yi Ming <ofseed@foxmail.com>
2024-08-31 11:58:58 +0000

    Enable xdg.portal by xdg-desktop-portal-gtk

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,18 @@
     };
   };
 
+  xdg.portal = {
+    enable = true;
+    config = {
+      common = {
+        default = [ "gtk" ];
+      };
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   environment = {
     systemPackages = with pkgs; [
       luajit
