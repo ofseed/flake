eaff33fb8ea7f318ea982ae6d350515318da728d
Yi Ming <ofseed@foxmail.com>
2024-08-31 07:46:44 +0000

    Update flake

--- flake.nix
+++ flake.nix
@@ -4,18 +4,26 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    catppuccin.url = "github:catppuccin/nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs = { nixpkgs, catppuccin, home-manager, ... }:
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      catppuccin,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       homeConfigurations."ofseed" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
