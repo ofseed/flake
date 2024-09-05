f406ce43674c97aae95c03191fb339a26e1a6802
Yi Ming <ofseed@foxmail.com>
2024-09-05 03:06:28 +0000

    Initialize nixos-wsl

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,18 +15,25 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       home-manager,
       catppuccin,
       ...
-    }:
+    }@attrs:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = attrs;
+        modules = [
+          ./configuration.nix
+        ];
+      };
       homeConfigurations."ofseed" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        pkgs = nixpkgs.legacyPackages.${system};
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
