c0b876758eaa16811081c8350225417cd9f33204
Yi Ming <ofseed@foxmail.com>
2024-09-10 03:25:08 +0800

    Add custom neovim directory to path

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,3 +1,13 @@
+# nvim
+set -gx NVIM_HOME "$HOME/neovim"
+if not string match -q -- "$NVIM_HOME/bin" $PATH
+  set -gx PATH "$NVIM_HOME/bin" $PATH
+end
+if not string match -q -- "$NVIM_HOME/share" $XDG_DATA_DIRS
+  set -gx XDG_DATA_DIRS "$NVIM_HOME/share" $XDG_DATA_DIRS
+end
+# nvim end
+
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
