2646edad74490d90d2b45754dfe94a2b8c4c025b
Yi Ming <ofseed@foxmail.com>
2024-09-16 03:25:26 +0800

    Add appindicator for GNOME

--- desktop/gnome.nix
+++ desktop/gnome.nix
@@ -7,6 +7,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
     gnomeExtensions.kimpanel
     gnomeExtensions.gsconnect
   ];
