ca8e539d1857368f0e50defdbe6634175ac5d196
Yi Ming <ofseed@foxmail.com>
2024-09-18 18:58:22 +0800

    Enable syncthing

--- os.nix
+++ os.nix
@@ -104,6 +104,7 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+  services.syncthing.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
