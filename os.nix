b7180b5a7462d7168ffa0bde3d8d9ef7fe9c13a3
Yi Ming <ofseed@foxmail.com>
2024-10-08 07:08:47 +0800

    Add udev rules for RPCS3 DualSense support

--- os.nix
+++ os.nix
@@ -135,6 +135,15 @@
     '';
   };
 
+  # For RPCS3 DualSense support
+  services.udev.extraRules = ''
+    # PS5 DualSense controller over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="0ce6", MODE="0666", TAG+="uaccess"
+
+    # PS5 DualSense controller over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*054C:0CE6*", MODE="0666", TAG+="uaccess"
+  '';
+
   services.syncthing.enable = true;
 
   # Open ports in the firewall.
