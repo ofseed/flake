9b68443449932ea03c5a1a8aeeb11bf8ae7df047
Yi Ming <ofseed@foxmail.com>
2025-02-11 21:08:47 +0800

    Add hyprland window manager

---
+++ desktop/hyprland.nix
@@ -0,0 +1,6 @@
+{ config, pkgs, ... }:
+{
+  programs.hyprland = {
+    enable = true;
+  };
+}
--- os.nix
+++ os.nix
@@ -10,6 +10,7 @@
     ./hardware-configuration.nix
     ./desktop/gnome.nix
     # ./desktop/plasma.nix
+    # ./desktop/hyprland.nix
     # ./desktop/niri.nix
   ];
 
